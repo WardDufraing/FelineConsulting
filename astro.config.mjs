024d1af6a73e1369b590b3cd79fccbcd37e1f68c
ward dufraing <warddufraing@wards-MacBook-Pro.local>
2025-04-12 23:22:44 +0200

    v2 - try 2 at fixing the ssr error

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,12 @@ import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({
+export default {
   output: 'server',
   adapter: cloudflare(),
-  integrations: [react()],
-});
+  integrations: [react({
+    // Disable SSR for React entirely
+    // @ts-ignore
+    ssr: false
+  })],
+};
